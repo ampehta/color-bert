b33f6a034e9d5c54c5ed1574cf7675cd222dab4a
Jake Tae <jaesungtae@gmail.com>
2021-03-05 23:08:54 +0900

    feature: create base dataset cls for mlm

--- dataset.py
+++ dataset.py
@@ -1,12 +1,17 @@
+import os
+
 from torch.utils.data import DataLoader, Dataset
 
 
 class ColorDataset(Dataset):
-    def __init__(self):
-        pass
+    def __init__(self, data_path, label_path=os.path.join("data", "labels.txt")):
+        with open(data_path) as f:
+            self.data = f.read().splitlines()
+        with open(label_path) as f:
+            self.labels = f.read().splitlines()
 
     def __len__(self):
-        pass
+        return len(self.data)
 
     def __getitem__(self, idx):
-        pass
+        return self.data[idx], self.labels[idx]
