4b767d1e1a20025baf8937da5b9fc93879189ad6
Jake Tae <jaesungtae@gmail.com>
2021-03-06 00:13:15 +0900

    feature: add cuda, cpu device toggle

--- train.py
+++ train.py
@@ -10,11 +10,14 @@ from utils import set_seed
 
 
 def train(model, tokenizer, data, labels, optimizer):
+    device = model.device
     optimizer.zero_grad()
-    tokens = tokenizer(data, return_tensors="pt", truncation=True, padding=True)
-    labels = tokenizer.batch_encode_plus(
-        labels, return_tensors="pt", truncation=True, padding=True
-    )["input_ids"]
+    tokens = tokenizer(data, return_tensors="pt", truncation=True, padding=True).to(
+        device
+    )
+    labels = tokenizer(labels, return_tensors="pt", truncation=True, padding=True)[
+        "input_ids"
+    ].to(device)
     loss = model(**tokens, labels=labels).loss
     loss.backward()
     optimizer.step()
@@ -24,15 +27,16 @@ def main(args):
     set_seed()
     model_name = args.model_name
     batch_size = args.batch_size
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForMaskedLM.from_pretrained(model_name)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    tokenizer = AutoTokenizer.from_pretrained(model_name).to(device)
+    model = AutoModelForMaskedLM.from_pretrained(model_name).to(device)
     optimizer = AdamW(model.parameters())
     mlm_dataloader = make_dataloader(os.path.join("data", "masked.txt"), batch_size)
     color_dataloader = make_dataloader(os.path.join("data", "color.txt"), batch_size)
     for i in range(args.num_epochs):
         # adaptive fine-tuning on color
         for data, labels in color_dataloader:
-            train(model, tokenizer, data, labels)
+            train(model, tokenizer, data, labels, device)
         # continued pre-training on mlm
         for data, labels in mlm_dataloader:
             train(model, tokenizer, data, labels)
